0c9067cefe22cfcbbf458d9d7b4464ae8a4ca24c
wpc <alex.hal9000@gmail.com>
2016-01-01 15:47:24 -0800

    add license header

--- src/test/java/com/thoughtworks/studios/journey/models/JourneyQueryTest.java
+++ src/test/java/com/thoughtworks/studios/journey/models/JourneyQueryTest.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of journey-neo4j-plugin. journey-neo4j-plugin is a neo4j server extension that provids out-of-box action path analysis features on top of the graph database.
+ *
+ * Copyright 2015 ThoughtWorks, Inc. and Pengchao Wang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.thoughtworks.studios.journey.models;
 
 import com.thoughtworks.studios.journey.ModelTestCase;
