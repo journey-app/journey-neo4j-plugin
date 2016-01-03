130d3231524a36e5caded2bd13fa1bf747fa8052
wpc <alex.hal9000@gmail.com>
2016-01-02 16:22:41 -0800

    allow set NEOSERVER for ssh deployment script

--- ssh-deploy.sh
+++ ssh-deploy.sh
@@ -20,7 +20,6 @@
 
 
 set -x
-NEOSERVER='remote-address'
 mvn clean install
 ssh -i ~/.ssh/mingle-dev.pem ec2-user@$NEOSERVER "sudo service neo4j stop"
 scp -i ~/.ssh/mingle-dev.pem target/journey-neo4j-plugin-1.0-SNAPSHOT-jar-with-dependencies.jar ec2-user@$SERVER:/usr/share/neo4j-community-2.2.3/plugins
