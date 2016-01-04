6c270aa5d295fcb0cd458d4a2c41555e81d0cb24
wpc <alex.hal9000@gmail.com>
2016-01-04 11:32:01 -0800

    fix ssh-deploy script

--- ssh-deploy.sh
+++ ssh-deploy.sh
@@ -22,5 +22,5 @@
 set -x
 mvn clean install
 ssh -i ~/.ssh/mingle-dev.pem ec2-user@$NEOSERVER "sudo service neo4j stop"
-scp -i ~/.ssh/mingle-dev.pem target/journey-neo4j-plugin-1.0-SNAPSHOT-jar-with-dependencies.jar ec2-user@$SERVER:/usr/share/neo4j-community-2.2.3/plugins
+scp -i ~/.ssh/mingle-dev.pem target/journey-neo4j-plugin-1.0-SNAPSHOT-jar-with-dependencies.jar ec2-user@$NEOSERVER:/usr/share/neo4j-community-2.2.3/plugins
 ssh -i ~/.ssh/mingle-dev.pem ec2-user@$NEOSERVER "sudo service neo4j start"
