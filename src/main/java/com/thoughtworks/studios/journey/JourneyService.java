21af61f8130e6f8acf1621504c3a7c3a6027c222
wpc <alex.hal9000@gmail.com>
2016-01-04 12:13:50 -0800

    Intellij trying to be too smart

--- src/main/java/com/thoughtworks/studios/journey/JourneyService.java
+++ src/main/java/com/thoughtworks/studios/journey/JourneyService.java
@@ -1,18 +1,18 @@
 /**
  * This file is part of journey-neo4j-plugin. journey-neo4j-plugin is a neo4j server extension that provides out-of-box action path analysis features on top of the graph database.
- * <p/>
+ *
  * Copyright 2015 ThoughtWorks, Inc. and Pengchao Wang
- * <p/>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p/>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p/>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
